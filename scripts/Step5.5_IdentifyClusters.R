c8301c190d1cc2de314b3c453973b242f2fb35d7
Mar Roca <m.rocacugat@student.maastrichtuniversity.nl>
2025-06-23 23:25:07 +0200

    The script to select a gene of interest now works!!
    wow such smart, it only took a lot of time

--- scripts/Step5.5_IdentifyClusters.R
+++ scripts/Step5.5_IdentifyClusters.R
@@ -2,8 +2,10 @@ library(dplyr)
 
 interest_genes <- c('CDKN1A', 'CDKN1B', 'CDKN1C', 'CDKN2A', 'CDKN2B', 'CDKN2C', 'CDKN2D')
 
-cluster <- RCy3::getTableColumns(columns = "GeneName,__glayCluster")
-cluster %>% filter_at(vars(GeneName), any_vars(. %in% c(interest_genes)))                                                                                                     
+genes.full <- RCy3::getTableColumns(columns = "GeneName,__glayCluster")
+genes.interest <- genes.full %>% filter_at(vars(GeneName), any_vars(. %in% c(interest_genes)))                                                                                                     
 
-RCy3::createSubnetwork(nodes = nodes.cluster$nodes, nodes.by.col = "shared name", subnetwork.name = paste0("PPI-cluster-", cluster))
+nodes.cluster <- RCy3::createColumnFilter('__glayCluster', '__glayCluster', unique(genes.interest[,2]), predicate = "IS")
 
+RCy3::createSubnetwork(nodes = nodes.cluster$nodes, nodes.by.col = "shared name", subnetwork.name = paste0("PPI-cluster-Interest"))
+exportImage(paste0(out.folder,'cluster-',cluster,'.png'), type='PNG', zoom=500)
