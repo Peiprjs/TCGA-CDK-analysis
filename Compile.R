497e0114f589c15ddf54fa552bd71bd7d2f3adfc
Mar Roca <m.rocacugat@student.maastrichtuniversity.nl>
2025-06-25 01:17:58 +0200

    Added compiler

---
+++ Compile.R
@@ -0,0 +1,10 @@
+rmarkdown::render("scripts/Step0_Preamble.R")
+rmarkdown::render("scripts/Step1_DataExploration.R")
+rmarkdown::render("scripts/Step2_EnrichmentAnalysiss.R")
+rmarkdown::render("scripts/Step3_PathwayVisualization.R")
+rmarkdown::render("scripts/Step4_PPINetwork.R")
+rmarkdown::render("scripts/Step5.5_IdentifyClusters.R")
+rmarkdown::render("scripts/Optional_GO-Process_PPI_DrugExtension.R")
+rmarkdown::render("scripts/Optional_Pathway_DrugExtension.R")
+
+system("mv scripts/*.html ../BBS2061-breast-cancer-pages")
