594aa2a97fb33280561ee977f9d33df91b338a2c
Mar Roca <m.rocacugat@student.maastrichtuniversity.nl>
2025-06-29 22:33:58 +0200

    CompileR modification

--- Compile.R
+++ Compile.R
@@ -8,7 +8,7 @@ rmarkdown::render("scripts/Step2_EnrichmentAnalysiss.R")
 rmarkdown::render("scripts/Step3_PathwayVisualization.R")
 rmarkdown::render("scripts/Step4_PPINetwork.R")
 rmarkdown::render("scripts/Step5_IdentifyClusters.R")
-rmarkdown::render("scripts/Step6-NumberDrugsTargetsTable.R")
+# rmarkdown::render("scripts/Step6-NumberDrugsTargetsTable.R")
 
 rmarkdown::render("scripts/Step0_Preamble.R")
 rmarkdown::render("scripts/Step1_DataExploration.R")
